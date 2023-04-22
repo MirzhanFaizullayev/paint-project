bd4e8c3de24872a3ff9091263da0881e3b95a59a
HARRY <mirzhan.faizullayev@narxoz.kz>
2023-04-22 12:51:35 +0600

    AA

---
+++ src/Paint.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Paint {
+    public static void main(String[] args) {
+        Scanner S= new Scanner();
+        int x=S.nextInt();
+      int  y=S.nextInt();
+        System.out.println(x+y);
+    }
+}
